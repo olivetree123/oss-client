8a6dca82efd7e8ed66083d0e27bd2ff1dd311627
gaojian <gaojian@g13125g.h3c.huawei-3com.com>
2021-11-03 14:47:00 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+from oss_client import AliyunClient
+
+client = AliyunClient(
+    access_key=ALY_ACCESS_KEY_ID,
+    secret_key=ALY_ACCESS_KEY_SECRET,
+    endpoint=ALY_OSS_ENDPOINT,
+    bucket=ALY_OSS_BUCKET,
+)
